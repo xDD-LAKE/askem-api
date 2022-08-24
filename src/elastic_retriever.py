c67c394c2c86c063e0d0ad375f46c8e702ee5886
Ian Ross <iross@cs.wisc.edu>
2022-08-24 09:27:38 -0500

    add retriever skeleton and deployment definitions for local dev

---
+++ src/elastic_retriever.py
@@ -0,0 +1,85 @@
+from retrieval.retriever import Retriever
+from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl.connections import connections
+from elasticsearch import RequestsHttpConnection
+from elasticsearch_dsl import Document, Text, connections, Integer, Float, Keyword, Join
+from elasticsearch.helpers import bulk
+import hashlib
+import pandas as pd
+import hashlib
+import logging
+
+logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def upsert(doc: Document) -> dict:
+    d = doc.to_dict(True)
+    d['_op_type'] = 'update'
+    d['doc'] = d['_source']
+    d['_index'] = doc.Index().name
+    d['doc_as_upsert'] = True
+    d['_id'] = str(doc['_id'])
+    del d['_source']
+    return d
+
+class GrometFN(Document):
+    cls = Text(fields={'raw': Keyword()})
+    detect_score = Float()
+    postprocess_score = Float()
+    dataset_id = Text(fields={'raw': Keyword()})
+    header_content = Text()
+    content = Text()
+    context_from_text = Text()
+    full_content = Text()
+    local_content = Text()
+    area = Integer()
+    pdf_name = Text(fields={'raw': Keyword()})
+    img_pth = Text(fields={'raw': Keyword()})
+
+
+class ElasticRetriever(Retriever):
+    def __init__(self, hosts=['localhost']):
+        self.hosts = hosts
+
+    def search(self, query: dict) -> dict:
+        connections.create_connection(hosts=self.hosts, timeout=20)
+        doc_filter = False
+        s = Search(index='eo-site')
+        s = s.query(query)
+        response = s.execute()
+        return response
+
+    def get_object(self, id: str):
+        if self.awsauth is not None:
+            connections.create_connection(hosts=self.hosts,
+                                          http_auth=self.awsauth,
+                                          use_ssl=True,
+                                          verify_certs=True,
+                                          connection_class=RequestsHttpConnection
+                                          )
+        else:
+            connections.create_connection(hosts=self.hosts)
+        try:
+            obj = GrometFN.get(id=id)
+        except:
+            obj = None
+        return obj
+
+    def build_index(self):
+        connections.create_connection(hosts=self.hosts)
+        logger.info('Building elastic index')
+        connections.create_connection(hosts=self.hosts)
+        GrometFN.init()
+        # TODO: Index documents.
+
+    def count(self, index:str):
+        connections.create_connection(hosts=self.hosts)
+        s = Search(index=index)
+        return s.count()
+
+    def delete(self):
+        raise NotImplementedError('TODO')
+
+    def rerank(self):
+        raise NotImplementedError('ElasticRetriever does not rerank results')
