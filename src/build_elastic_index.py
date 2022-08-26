ed86342160581f74e42692d7c3c2413adb2e94aa
Ian Ross <iross@cs.wisc.edu>
2022-08-26 09:23:11 -0500

    pass data  dir around

--- src/build_elastic_index.py
+++ src/build_elastic_index.py
@@ -3,11 +3,12 @@ from elastic_retriever import ElasticRetriever
 import os
 
 @click.command()
+@click.option('--input_dir', type=str, help='', default='/input/')
 @click.option('--host', type=str, help='', default='localhost')
-def run(host):
+def run(host, input_dir):
     ret = ElasticRetriever(hosts=[host])
     print('Connected to retriever, building indices')
-    ret.build_index()
+    ret.build_index(input_dir)
     print('Done building index')
 
 if __name__ == '__main__':
